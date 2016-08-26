50646c01e67be75fd7394e0df2afa4599b586b4d
scott-marinoff <scott.marinoff@mixpanel.com>
2016-08-26 14:35:26 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -5,8 +5,11 @@
  <body>
  	<?php echo '<p>Hello World</p>'; ?> 
 
+<!-- 
  	<?php require_once("/mixpanel-php-master/lib/Mixpanel.php"); ?> 
- 		  require_once("/path/to/vendor/mixpanel/mixpanel-php/lib/Mixpanel.php");
+ -->
+
+	<?php require "/mixpanel-php-master/lib/Mixpanel.php"; ?> 
 
 <!-- 	// get the Mixpanel class instance, replace with your project token
 	$mp = Mixpanel::getInstance("fd57ae8ed4617b21b3ee19238a7309d8");
