0c8bb2c1158ad2076873bd5a0d2e01f90f7d233b
scott-marinoff <scott.marinoff@mixpanel.com>
2016-08-26 14:22:53 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,25 +3,25 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ 	<?php echo '<p>Hello World</p>'; 
+ 	
+	// import Mixpanel
+	require 'mixpanel-php/lib/Mixpanel.php';
 
- <?php
-// import Mixpanel
-require 'mixpanel-php/lib/Mixpanel.php';
+	// get the Mixpanel class instance, replace with your project token
+	$mp = Mixpanel::getInstance("fd57ae8ed4617b21b3ee19238a7309d8");
 
-// get the Mixpanel class instance, replace with your project token
-$mp = Mixpanel::getInstance("fd57ae8ed4617b21b3ee19238a7309d8");
+	// track an event
+	$mp->track("button clicked", array("label" => "sign-up"));
 
-// track an event
-$mp->track("button clicked", array("label" => "sign-up"));
+	// create/update a profile for user id 12345
+	$mp->people->set(12345, array(
+	    '$first_name'       => "John",
+	    '$last_name'        => "Doe",
+	    '$email'            => "john.doe@example.com",
+	    '$phone'            => "5555555555",
+	    "Favorite Color"    => "red"
+	)); ?> 
 
-// create/update a profile for user id 12345
-$mp->people->set(12345, array(
-    '$first_name'       => "John",
-    '$last_name'        => "Doe",
-    '$email'            => "john.doe@example.com",
-    '$phone'            => "5555555555",
-    "Favorite Color"    => "red"
-)); ?> 
  </body>
 </html>
