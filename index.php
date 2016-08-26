23913576b7b059bd312af1064f7e608bc7b3baee
scott-marinoff <scott.marinoff@mixpanel.com>
2016-08-26 14:27:02 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,18 +3,11 @@
   <title>PHP Test</title>
  </head>
  <body>
- 	<?php echo '<p>Hello World</p>'; 
+ 	<?php echo '<p>Hello World</p>'; ?> 
 
-	// import Mixpanel
-	require 'mixpanel-php/lib/Mixpanel.php';
 
-	// get the Mixpanel class instance, replace with your project token
-	$mp = Mixpanel::getInstance("fd57ae8ed4617b21b3ee19238a7309d8");
-
-	// track an event
-	$mp->track("Page viewed", array("Page" => "Homepage"));
 	
-	?> 
+	
 
  </body>
 </html>
